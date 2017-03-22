0b5f0e6d90179b917f8f9a2f86b003b81372e47e
ryan <ryanmarchock@gmail.com>
2017-03-22 14:17:17 +0000

    created test for game view

---
+++ test/js/modules/hand-game/start-view/start-view.spec.js
@@ -0,0 +1,10 @@
+import StartView from '../../../../../src/js/modules/hand-game/start-view/start-view';
+
+describe('Game StartView', () => {
+
+  it('should exist', () => {
+
+    expect(StartView).to.exist;
+
+	});
+});
