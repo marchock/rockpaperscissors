bc653a9d684801514eec9e1ef0e0893e8c47ea0b
ryan <ryanmarchock@gmail.com>
2017-03-23 13:46:31 +0000

    updated code comments and tests

--- src/js/modules/hand-game/start-view/start-view.js
+++ src/js/modules/hand-game/start-view/start-view.js
@@ -1,20 +1,55 @@
 import html from './start-view.html';
 import { settings } from '../hand-game-settings';
 
+/* Loads and start-veiw.html into the DOM
+ * @Class
+ */
 class StartView {
 
+  /* Create a start view
+   * @param: {object} ele
+   */
   constructor(ele) {
+    /* A reference where to inject the start-view.html
+     * @object
+     */
     this.$ele = ele;
+
+    /* a copy of start-view.html
+     * @object
+     */
     this.html = html;
+
+    /* Reference for hand-game-settings.js
+     * @object
+     */
     this.settings = settings;
   }
 
+  /* inject start-view.html into the DOM
+   * @param {object} ref
+   * @return void
+   */
   loadView(ref) {
+    /* Reference for handGame class
+     * @Object
+     */
     this._handGame = ref;
 
+    /* Injecting HTML into the DOM reference
+     * @object
+     */
     this.$ele.innerHTML = this.html;
 
-    this.$ele.querySelector('form').addEventListener('submit', (evt) => {
+    this.setupFormEvent();
+  }
+
+  /* Setup form submit event to update game settings
+   * @return void
+   */
+  setupFormEvent() {
+    let form = this.$ele.querySelector('form');
+    form.addEventListener('submit', (evt) => {
       evt.preventDefault();
 
       let inputs = this.$ele.querySelectorAll('input');
@@ -30,11 +65,14 @@ class StartView {
     })
   }
 
+  /* Update game settings and then use reference to load game view
+   * @param {array} checked
+   * @return void
+   */
   updateSettings(checked) {
     this.settings.setGameType(checked[0].value);
     this.settings.setBestOf(Number(checked[2].value));
     this.settings.setVerses(checked[1].value);
-
     this._handGame.loadGameView();
   }
 }
--- test/js/modules/hand-game/start-view/start-view.spec.js
+++ test/js/modules/hand-game/start-view/start-view.spec.js
@@ -1,10 +1,39 @@
 import StartView from '../../../../../src/js/modules/hand-game/start-view/start-view';
 
 describe('Game StartView', () => {
+  let $ele = null;
+  let startView = null;
+
+  beforeEach(function() {
+    $ele = document.getElementById('hand-game');
+    startView = new StartView($ele);
+  });
 
   it('should exist', () => {
-
     expect(StartView).to.exist;
-
 	});
+
+  describe('this.html', () => {
+    it('should be an object', () => {
+      setTimeout(() => {
+        expect(startView.html).to.be.an('object');
+      }, 100);
+    });
+  });
+
+  describe('this.$ele', () => {
+    it('should be an object', () => {
+      setTimeout(() => {
+        expect(startView.$ele).to.be.an('object');
+      }, 100);
+    });
+  });
+
+  describe('this.settings', () => {
+    it('should be an object', () => {
+      setTimeout(() => {
+        expect(startView.settings).to.be.an('object');
+      }, 100);
+    });
+  });
 });
